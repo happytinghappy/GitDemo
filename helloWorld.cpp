cf27146ebcadfd0b1649999c38f4677317a24850
happytinghappy <153181044+happytinghappy@users.noreply.github.com>
2023-12-07 20:42:01 +0800

    Update.helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 // Todo: 寫一個印出"Hello World"的程式
 int main() {
-    cout  << "Hello World" << "\n";
+    for(int i = 0; i < 50; i++){
+        cout  << "Hello World" << "\n";
+    }
     return 0;
 }
